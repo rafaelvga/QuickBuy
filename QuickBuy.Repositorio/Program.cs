07d955589be5bfdfed9290156170874391bc4cb5
Rafael Rodrigues de Souza <rafaelvga@hotmail.com>
2019-05-15 17:47:44 -0300

    Inicial

---
+++ QuickBuy.Repositorio/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuickBuy.Repositorio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
