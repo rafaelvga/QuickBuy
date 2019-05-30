98e8e5c471303275539a698bb8d0a9afd0a464be
Rafael Rodrigues de Souza <rafaelvga@hotmail.com>
2019-05-30 18:21:50 -0300

    Alterações e criação das classes

---
+++ QuickBuy.Dominio/Entidades/Produto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.Entidades
+{
+    class Produto:Entidade
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+        public decimal Preco { get; set; }
+    }
+}
---
+++ QuickBuy.Dominio/ObjetoDeValor/FormaPagamento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QuickBuy.Dominio.ObjetoDeValor
+{
+    class FormaPagamento:Entidades.Entidade
+    {
+        public string Nome { get; set; }
+        public string Descricao { get; set; }
+
+        public bool EhBoleto {
+            get { return Id == (int)TipoFormaPagamentoEnum.Boleto; }
+        }
+        public bool EhCartao
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.CartaoCredito; }
+        }
+        public bool EhDeposito
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.Deposito; }
+        }
+        public bool NaoFoiDefinido
+        {
+            get { return Id == (int)TipoFormaPagamentoEnum.NaoDefinido; }
+        }
+    }
+}
